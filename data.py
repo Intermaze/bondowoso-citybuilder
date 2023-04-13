c4c36c09b91e00b8e884174337a29047feeb4bda
Intermaze <rafikiprawhira@gmail.com>
2023-04-13 17:58:35 +0700

    Mengubah csv ke array
    Berhasil mengubah csv ke array;
    mengubah nama fungsi load() menjadi csv_to_array() karena spesifikasi berbeda dengan F13 - Load

--- data.py
+++ data.py
@@ -1,5 +1,69 @@
-import argparse
-    #...
 
-def load(lokasi_fisik, variabel_target):
+def jumlah_elemen(line_csv):
+    #line merupakan string
+
+    count = 0
+    for i in range(len(line_csv)):
+        if line_csv[i] == ";":
+            count += 1
+
+    #jumlah elemen dalam 1 line csv merupakan jumlah ";" ditambah 1
+    return count+1
+
+#Fungsi append buatan
+def append_arr2_to_arr1(arr1, arr2, N1, N2):
+    #N1 dan N2 adalah panjang array
+    arr3 = [0 for i in range(N1+N2)]
+
+    for i in range(N1):
+        arr3[i] = arr1[i]
+    for i in range(N2):
+        arr3[N1+i] = arr2[i]
+
+    return arr3
+
+def combine_arr2_to_arr1(arr1, arr2, N1):
+    #N1 dan N2 adalah panjang array
+    arr3 = [0 for i in range(N1+1)]
+
+    for i in range(N1):
+        arr3[i] = arr1[i]
+    arr3[N1] = arr2
+
+    return arr3
+
     
+
+def csv_to_array(lokasi_fisik, variabel_target):
+
+    #Inisialisasi
+    file = open(lokasi_fisik, 'r')
+    n1 = 0 #jumlah elemen variabel_target
+
+    for line in file:
+        line = line.rstrip() #Menghilangkan "\n"
+
+        arr_line = [0 for i in range(jumlah_elemen(line))]
+
+        temp = ""
+        element_count = 0
+        for i in range(len(line)):
+            if line[i] != ";":
+                temp += line[i]
+            else:
+                arr_line[element_count] = temp
+                element_count += 1
+                temp = ""
+
+        #Ada elemen lagi, karena tidak ada ";" di akhir line
+        arr_line[element_count] = temp
+
+        variabel_target = combine_arr2_to_arr1(variabel_target, arr_line, n1)
+        n1 += 1
+    
+    return variabel_target
+
+
+    
+
+
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # File: main.py
-from data import load
+from data import csv_to_array
 import commands
 
 # Anggap semua fungsi yang dipanggil merupakan fungsi yang sudah dibuat sendiri pada modul lain
@@ -7,9 +7,11 @@ users = [] # Matriks data user
 candi = [] # Matriks data candi
 bahan_bangunan = [] # Data bahan bangunan
 
-load("file/user.csv", users)
-load("file/candi.csv", candi)
-load("file/bahan_bangunan.csv", bahan_bangunan)
+users = csv_to_array("save/user.csv", users)
+candi = csv_to_array("save/candi.csv", candi)
+bahan_bangunan = csv_to_array("save/bahan_bangunan.csv", bahan_bangunan)
+
+print(users)
 
 while True:
   masukan = input(">>> ")
